d05555671c0e9360936608f93343575a5ab35e48
jemimahmartinez <martinezjemimah@gmail.com>
2021-02-11 19:33:24 +1300

    Tested with my own laptop

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React hi
+          Learn React hihihiihiihiii
         </a>
       </header>
     </div>
